7f2800454b95e3e5c5cae11cb1b8433fb0f0b29f
JaimeAlvaradoValiente <javierromero236@gmail.com>
2023-03-28 17:05:04 +0000

    Update from https://github.com/javierrome236/quantumDeployment/commit/d4aeec0f3b731a78808c5f966b84461bfbe95550

---
+++ SimonIBMquirk.py
@@ -0,0 +1,20 @@
+from qiskit import execute, QuantumRegister, ClassicalRegister, QuantumCircuit, Aer
+from numpy import pi
+qreg_q = QuantumRegister(6, 'q')
+creg_c = ClassicalRegister(6, 'c')
+circuit = QuantumCircuit(qreg_q, creg_c)
+circuit.h(qreg_q[0])
+circuit.h(qreg_q[1])
+circuit.h(qreg_q[2])
+circuit.h(qreg_q[0])
+circuit.h(qreg_q[1])
+circuit.h(qreg_q[2])
+circuit.measure(qreg_q[0], creg_c[0])
+circuit.measure(qreg_q[1], creg_c[1])
+circuit.measure(qreg_q[2], creg_c[2])
+backend = Aer.get_backend("qasm_simulator")
+x=int(shots)
+job = execute(circuit, backend, shots=x)
+result = job.result()
+counts = result.get_counts()
+return counts

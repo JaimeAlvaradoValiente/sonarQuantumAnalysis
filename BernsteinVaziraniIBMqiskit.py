8a206671d60239f4b448742e216b5dedb36f5365
JaimeAV <74024882+JaimeAlvaradoValiente@users.noreply.github.com>
2023-03-28 17:24:28 +0200

    Update BernsteinVaziraniIBMqiskit.py

--- BernsteinVaziraniIBMqiskit.py
+++ BernsteinVaziraniIBMqiskit.py
@@ -1,14 +1,15 @@
-from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
-
-qr = QuantumRegister(3, 'q')
-anc = QuantumRegister(1, 'ancilla')
-cr = ClassicalRegister(3, 'c')
-qc = QuantumCircuit(qr, anc, cr)
-
-qc.x(anc[0])
-qc.h(anc[0])
-qc.h(qr[0:3])
-qc.cx(qr[0:3], anc[0])
-qc.h(qr[0:3])
-qc.barrier(qr)
-qc.measure(qr, cr)
+from qiskit import execute, QuantumRegister, ClassicalRegister, QuantumCircuit, Aer
+from numpy import pi
+qreg_q = QuantumRegister(5, 'q')
+creg_c = ClassicalRegister(5, 'c')
+circuit = QuantumCircuit(qreg_q, creg_c)
+circuit.h(qreg_q[0])
+circuit.h(qreg_q[1])
+circuit.h(qreg_q[2])
+circuit.h(qreg_q[3])
+circuit.h(qreg_q[4])
+circuit.measure(qreg_q[0], creg_c[0])
+circuit.measure(qreg_q[1], creg_c[1])
+circuit.measure(qreg_q[2], creg_c[2])
+circuit.measure(qreg_q[3], creg_c[3])
+circuit.measure(qreg_q[4], creg_c[4])

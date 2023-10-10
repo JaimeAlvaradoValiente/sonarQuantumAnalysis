3ac959a0c67ca00eb0c0a9828548acc611abb1b3
JaimeAlvaradoValiente <javierromero236@gmail.com>
2023-10-10 12:09:06 +0000

    Update from https://github.com/javierrome236/quantumDeployment/commit/f81273b237bce1d21bd6b969af70c1e051b5dba2

---
+++ Range1IBMqiskit.py
@@ -0,0 +1,37 @@
+from qiskit import QuantumCircuit, execute, Aer
+from qiskit import QuantumRegister, ClassicalRegister
+
+
+# Circuito rango (1,4)=[2,3]
+qreg_q = QuantumRegister(3, 'q')
+creg_c = ClassicalRegister(3, 'c')
+
+# Crea el circuito
+rango_1_4 = QuantumCircuit(qreg_q, creg_c)
+
+rango_1_4.x(qreg_q[2])
+rango_1_4.z(qreg_q[2])
+
+rango_1_4.x(qreg_q[1])
+
+rango_1_4.cz(qreg_q[1], qreg_q[2])
+
+rango_1_4.x(qreg_q[1])
+rango_1_4.x(qreg_q[2])
+
+rango_1_4.measure(qreg_q, creg_c)
+
+backend = Aer.get_backend("qasm_simulator")
+shots = int(1024)
+job = execute(rango_1_4, backend, shots=shots)
+result = job.result()
+counts = result.get_counts()
+
+return counts
+
+
+    
+
+
+
+
